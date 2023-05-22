80e2341df5a85f8f78862aa04d4b189f13a0fb23
sank676 <ssankari676@gmail.com>
2023-05-22 14:15:03 +0530

    21-05-2000

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -2,9 +2,18 @@ package com.example.demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Demo1Application {
+
+	@GetMapping("/hello")
+	public String message()
+	{
+		return "welcome to my home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
